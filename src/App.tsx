de304a1a1412ab6ab1c4b4970652f3056daaa878
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-12-25 18:33:16 -0300

    Optimizing the application making it more performant using memo and useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 
 
@@ -18,18 +18,18 @@ import './styles/content.scss';
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  
 
-  function handleClickButton(id: number) {
+
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, [])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      
-      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId}/>
-      <Content selectedGenreId={selectedGenreId}/>
-      
+
+      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
+      <Content selectedGenreId={selectedGenreId} />
+
     </div>
   )
 }
